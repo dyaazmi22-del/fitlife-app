9c6e1916941f6a65593b41174b0998cb76230260
dyaazmi22-del <dyaazmi22@gmail.com>
2026-06-28 23:30:40 +0700

    Create main.tsx

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import FitLifeDashboard from './fitlife_dashboard.tsx'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <FitLifeDashboard />
+  </React.StrictMode>,
+)
